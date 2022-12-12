a829cca9da4af488bd4ba5de31d471d89f195df7
wahrme <wahrme@kth.se>
2022-12-12 10:26:09 +0100

    changed gitignore, formatted searchResults.js

---
+++ src/searchResults.js
@@ -0,0 +1,61 @@
+import { API_KEY_GEOCODE, API_KEY_YOUTUBE } from "./apiConfig.js";
+
+export const search = () => {
+  var element = document.getElementById("video")
+  var newLine = "\r\n"
+  var searchInput = document.getElementById("searchInput").value
+  const coordinates = fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + searchInput + "&key=" + API_KEY_GEOCODE)
+    .then(response => { return response.json() })
+    .then(data => { return data.results[0].geometry.location });
+
+  console.log(coordinates)
+
+  var x = undefined;
+  var y = undefined;
+  var radius = 25;
+  var radiusType = "km";
+  var maxResults = 5;
+
+  const printAddress = () => {
+    coordinates.then((data) => {
+      console.log(data);
+      x = String(data.lat);
+      y = String(data.lng);
+      console.log(x);
+      console.log(y);
+
+      fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&location=" + x + "%2C%20" + y
+        + "&locationRadius=" + radius + radiusType + "&maxResults=" + maxResults + "&type=video&key=" + API_KEY_YOUTUBE)
+        .then((result) => { return result.json() })
+        .then((data) => {
+          console.log(data)
+          let videos = data.items
+          for (var video of videos) {
+
+            var url = "https://www.youtube.com/watch?v=" + video.id.videoId;
+            var a = document.createElement('a');
+
+            // Create the text node for anchor element.
+            var link = document.createTextNode(video.snippet.title);
+
+            // Append the text node to anchor element.
+            a.appendChild(link);
+
+            // Set the title.
+            a.title = "Stream";
+
+            // Set the href property.
+            a.href = url;
+
+            // Append the anchor element to the body.
+            element.appendChild(a);
+            element.appendChild(newLine);
+          }
+
+        })
+    });
+  };
+
+  printAddress();
+
+}
